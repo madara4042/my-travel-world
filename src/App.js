dc2a93ced891476a39535d48706eb41bd2bd1eb5
Ilya <ilya.yakubovski.99@mail.ru>
2021-05-27 18:18:04 +0200

    castomization setup tailwind

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import "tailwindcss/tailwind.css"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         
-      <h1 className="text-red-500 text-4xl text-center">Hello world</h1>
+      <h1 className="text-blue-100 text-4xl text-center">Hello world</h1>
         <div className=" border-2 w-1/2 h-screen">DIV</div>
       </header>
     </div>
